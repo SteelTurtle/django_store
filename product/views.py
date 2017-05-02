9c18e035cc7355ed2168adbb1751047c4ebf8c28
Daniele <steelturtle@msn.com>
2017-05-02 16:11:56 +0900

    started playing with views

--- product/views.py
+++ product/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.http.response import HttpResponse
+from django.template import loader
+
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    template = loader.get_template('product/tag_list.html')
+    output = template.render({'tag_list': tag_list})
+    return HttpResponse(output)
+
+
+def tag_detail(request):
+    pass
