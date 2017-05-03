a24f2ab6f2e4b762463a2739a097c2fe39e5c0aa
Daniele <steelturtle@msn.com>
2017-05-03 16:40:37 +0900

    I hate regular expressions in urls, really...

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
-from blog.views import post_list
+from blog.views import post_detail, post_list
 
 urlpatterns = [
     url(r'^$', post_list, name='blog_post_list'),
+    url(r'^(?P<year>\d{4})/'
+        r'^(?P<month>\d{1,2})/'
+        r'^(?P<slug>[\w\-]+)/$',
+        post_detail, name='blog_post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
+
+
+def post_detail(request, year, month, slug):
+    post = get_object_or_404(Post.objects
+                             .filter(publication_date__year=year)
+                             .filter(publication_date__month=month)
+                             .get(slug__iexact=slug))
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_list(request):
