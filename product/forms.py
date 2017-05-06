914a2a54f161df48cad8da73357cfbd24f8b9f68
Daniele <steelturtle@msn.com>
2017-05-06 14:51:16 +0900

    Added clean methods on the forms

--- product/forms.py
+++ product/forms.py
@@ -1,10 +1,32 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Tag
+from .models import Link, Product, Tag
 
 
-class TagForm(forms.ModelForm):
+class SlugCleanMixin:
+    """Mixin Class for the common "clean_slug" methods in the forms."""
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError('Slug cannot be "create". That is a reserved word')
+        return new_slug
+
+
+class LinkForm(forms.ModelForm):
+    class Meta:
+        model = Link
+        fields = '__all__'
+
+
+class ProductForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class TagForm(SlugCleanMixin, forms.ModelForm):
     # inherit all the Tag's model fields
     class Meta:
         model = Tag
@@ -13,9 +35,3 @@ class TagForm(forms.ModelForm):
     # by convention, because this form is a ModelForm, all the clean methods must start with "clean_"
     def clean_name(self):
         return self.cleaned_data['name'].lower()
-
-    def clean_slug(self):
-        new_slug = self.cleaned_data['slug'].lower()
-        if new_slug == 'create':
-            raise ValidationError('Slug cannot be "create". That is a reserved word')
-        return new_slug
