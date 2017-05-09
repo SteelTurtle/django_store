43146b754ea36a066770e3e6fb5e7c1c25056cc6
Daniele <steelturtle@msn.com>
2017-05-09 17:07:54 +0900

    Implementing GCBViews... Putting the fan out of development.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views import View
+from django.views.generic import CreateView, ListView, View
 
 from .forms import PostForm
 from .models import Post
@@ -13,31 +13,13 @@ def post_detail(request, year, month, slug):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
-class PostList(View):
-    def get(self, request):
-        return render(request, 'blog/post_list.html', {'post_list': Post.objects.all()})
+class PostList(ListView):
+    model = Post
 
 
-class PostCreate(View):
+class PostCreate(CreateView):
     form_class = PostForm
     template_name = 'blog/post_form.html'
-
-    # GET
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class()})
-
-    # POST
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        # posted bound_form data are valid
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            # redirect uses the "get_absolute_url" method of the newly created Post instance
-            # to redirect the user to the post_detail.html page of "new_post"
-            return redirect(new_post)
-        # posted bound_form data are invalid
-        else:
-            return render(request, self.template_name, {'form': bound_form})
 
 
 class PostDelete(View):
--- product/views.py
+++ product/views.py
@@ -1,25 +1,15 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views import View
+from django.views.generic import CreateView, DeleteView, UpdateView, View
 
 from .forms import LinkForm, ProductForm, TagForm
 from .models import Link, Product, Tag
 
 
-class LinkCreate(View):
+class LinkCreate(CreateView):
     form_class = LinkForm
-    template_name = 'product/product_link_form.html'
-
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class()})
-
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_link = bound_form.save()
-            return redirect(new_link)
-        else:
-            return render(request, self.template_name, {'form': bound_form})
+    template_name = 'product/link_form.html'
 
 
 class LinkUpdate(View):
@@ -45,29 +35,20 @@ class LinkUpdate(View):
 
 
 class LinkDelete(View):
-    pass
+    def get(self, request, pk):
+        link = get_object_or_404(Link, pk=pk)
+        return render(request, 'product/link_confirm_delete.html', {'link': link})
+
+    def post(self, request, pk):
+        link = get_object_or_404(Link, pk=pk)
+        product = link.product
+        link.delete()
+        return redirect(product)
 
 
-class ProductCreate(View):
+class ProductCreate(CreateView):
     form_class = ProductForm
     template_name = 'product/product_form.html'
-
-    # GET
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class()})
-
-    # POST
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        # posted bound_form data are valid
-        if bound_form.is_valid():
-            new_product = bound_form.save()
-            # redirect uses the "get_absolute_url" method of the newly created Product instance
-            # to redirect the user to the tag_detail.html page of "new_tag"
-            return redirect(new_product)
-        # posted bound_form data are invalid
-        else:
-            return render(request, self.template_name, {'form': bound_form})
 
 
 def product_detail(request, slug):
@@ -76,8 +57,9 @@ def product_detail(request, slug):
     return render(request, template, {'product': product})
 
 
-class ProductDelete(View):
-    pass
+class ProductDelete(DeleteView):
+    model = Product
+    success_url = reverse_lazy('product_product_list')
 
 
 class ProductList(View):
@@ -122,35 +104,21 @@ class ProductList(View):
         return render(request, self.template_name, context)
 
 
-class ProductUpdate(View):
-    pass
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product/product_form_update.html'
 
 
 # A CBV implicitly returns HTTP code 405 if none of the GET or POST request methods are matched
-class TagCreate(View):
+class TagCreate(CreateView):
     form_class = TagForm
     template_name = 'product/tag_form.html'
 
-    # GET
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class()})
 
-    # POST
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        # posted bound_form data are valid
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            # redirect uses the "get_absolute_url" method of the newly created Tag instance
-            # to redirect the user to the tag_detail.html page of "new_tag"
-            return redirect(new_tag)
-        # posted bound_form data are invalid
-        else:
-            return render(request, self.template_name, {'form': bound_form})
-
-
-class TagDelete(View):
-    pass
+class TagDelete(DeleteView):
+    model = Tag
+    success_url = reverse_lazy('product_tag_list')
 
 
 def tag_detail(request, slug):
@@ -164,5 +132,7 @@ def tag_list(request):
     return render(request, template, {'tag_list': Tag.objects.all()})
 
 
-class TagUpdate(View):
-    pass
+class TagUpdate(UpdateView):
+    form_class = TagForm
+    model = Tag
+    template_name = 'product/tag_form_update.html'
